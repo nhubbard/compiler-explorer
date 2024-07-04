2385a36df3a6bac28afc6129c7a7c8e6848624af
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2024-07-04 11:06:42 -0500

    feature/power-asm: Add BCTAR/BCTARL instruction documentation URL

--- lib/asm-docs/generated/asm-docs-power.ts
+++ lib/asm-docs/generated/asm-docs-power.ts
@@ -239,9 +239,10 @@ export function getAsmOpcode(opcode: string | undefined): AssemblyInstructionInf
                     <p>The <strong>bctar</strong> and <strong>bctarl</strong> instructions branch to the address stored in the Branch Target Address Register (TAR) if a specified condition is met. The condition is determined by the Condition Register (CR) bit specified by <em>BI+32</em> and the BO field.</p>
                     <p>The <em>BO</em> field is used to resolve the branch as described in the instruction set architecture, and the <em>BH</em> field specifies the context of the branch. The branch target address is formed by concatenating bits 0-61 of the TAR with <code>0b00</code>. In 32-bit mode, the high-order 32 bits of the branch target address are set to 0.</p>
                     <p>If <em>LK</em> is 1, then the effective address of the instruction following the branch instruction is placed into the Link Register (LR).</p>
+                    <p>The instruction is documented on page 78 of the linked PDF file.</p>
                 `,
                 "tooltip": "Branch Conditional to Branch Target Address",
-                "url": ""
+                "url": "https://files.openpower.foundation/s/9izgC5Rogi5Ywmm"
             };
         case "CLCS":
             return {
